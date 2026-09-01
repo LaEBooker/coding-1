a6779f8d9770c3631d34a992482dea66e8b57bc7
LaEBooker <ebooker834@boyslatin.net>
2026-09-01 14:25:42 +0000

    data type

---
+++ unit 1/detatypes.py
@@ -0,0 +1,32 @@
+# STRINGS- charaters that are wrapped within quotes.
+# CHARACTERS- letters, numbers, and symbols.
+# you can use both single and double quotations.
+# marks for strings
+
+"Elijah Booker
+
+"123 Main street
+
+"this cost $15.00
+
+# the hastag is a marker that makes anything behind it invisible.
+
+# INTERGERS- data types that are just whole numbers.
+
+10
+100
+1000
+10000
+
+# FLOATS- A number with a decimal.
+
+20.99
+45.89
+
+# BOOLEAN- a data type that can only be either true or false
+# we do not understand quotations for boolean values.
+# the computer understands these words as something unique.
+
+False, 0, "",
+True, 1, "huksgjmbisuj"
+
